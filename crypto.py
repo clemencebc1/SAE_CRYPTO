2d3ae797a5ac7ba684fbdd66d49ba0fb86f36ca5
Clemence <clemence.bcq@gmail.com~>
2024-10-10 12:14:27 +0200

    ajout de la methode passage_pourcentage

---
+++ crypto.py
@@ -0,0 +1,22 @@
+
+from constantes import *
+
+class Substition:
+    def passage_pourcentage(self, dictionnaire_apparation, 
+                            longueur):
+        """a partir d'un dictionnaire d'occurence
+        d'apparition d'une lettre, calcule la frequence
+        d'apparition dans le mot
+
+        Args:
+            dictionnaire_apparation (dict): dico d'occurence
+            longueur (int): longueur du message
+
+        Returns:
+            dict: dico de frequence
+        """
+        dictionnaire_frequence = dict()
+        for key,value in dictionnaire_apparation.items():
+            dictionnaire_frequence[key]=value*100/longueur
+        return dictionnaire_frequence
+            
