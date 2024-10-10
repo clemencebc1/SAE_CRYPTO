ddb4bf663d6d8e430609fbbb18a95e1b56356210
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2024-10-10 16:36:25 +0200

    Decryptage du message de manière naive + ajout fonction longueur_message et init() modifié

--- crypto.py
+++ crypto.py
@@ -4,10 +4,14 @@ from constantes import *
 class Substition:
 
     def __init__ (self) :
-        self.fichier = open("message1_chiffre.txt", "r")
+        fic = open("message1_chiffre.txt", "r", encoding="utf-8")
+        self.fichier = []
+        for line in fic.readlines():
+            line.rstrip('\n')
+            self.fichier.append(line)
 
     def passage_pourcentage(self, dictionnaire_apparation, 
-                            longueur):
+                            longueur) -> dict :
         """a partir d'un dictionnaire d'occurence
         d'apparition d'une lettre, calcule la frequence
         d'apparition dans le mot
@@ -24,10 +28,14 @@ class Substition:
             dictionnaire_frequence[key]=value*100/longueur
         return dictionnaire_frequence
             
-    def frequence_lettres(self):
+    def frequence_lettres(self) -> dict:
+        """ Détermine la fréquence d'apparition d'une lettre dans le message
+
+        Returns:
+            dict: le dictionnaire d'occurence
+        """        
         dico_apparition = {}
-        mes = self.fichier.split("\n")
-        for line in mes:
+        for line in self.fichier :
             for char in line :
                 # Est une lettre de l'alphabet
                 if char.isalpha():
@@ -36,10 +44,50 @@ class Substition:
                     else :
                         dico_apparition[char] += 1
         return dico_apparition
-        
-    def compare_frequence_lettres(self):
-        
+    
+    
+    def longueur_message(self):
+        """ Calcule la longueur totale du message, c'est à dire le nombre de caractère
+        """
+        longueur = 0
+        for line in self.fichier:
+            longueur += len(line)
+        return longueur
+
+    
+    def decrypt(self, cle : int) -> str:
+        """ Déchiffre le message en utilisant la clé de substitution
+
+        Args:
+            cle (int): le nombre de décalage de l'alphabet
+
+        Returns:
+            str: le message déchiffré
+        """
+        res = ""
+        for line in self.fichier:
+            for char in line :
+                if not char.isalpha(): 
+                    res += char
+                    continue
+
+                index = ALPHABET.index(char.lower()) # index de la lettre dans l'alphabet
+                nv_char = ALPHABET[(index - cle) % ALPHABET_SIZE] # nouveau caractère (index - clé modulo 26) 
+
+                res += nv_char
+        return res
     
 
+    # décryptage temporaire sans l'utilisation des fréquences
+    def decryptage_naif(self):
+        """ Déchiffre un message par substitution jusqu'à trouver un message cohérent (version naive)
 
-
+        Returns:
+            str: le message déchiffré correct
+        """
+        for cle in range(1, ALPHABET_SIZE):
+            message = self.decrypt(cle)
+            print(message)
+            # appeler la fonction qui vérifie la cohérence du message
+            # if correct :
+            # return message
