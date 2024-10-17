49173ca9b732829187e5f10942db41cf3d6ccf47
Yasin <keskan543@gmail.com>
2024-10-17 20:55:03 +0200

    Docstring + PEP8

--- dechiffrement.py
+++ dechiffrement.py
@@ -1,42 +1,60 @@
-
 import os
 import constantes
 import crypto
 
-def open_world_list(path:str) -> list[str]:
+def open_world_list(path: str) -> list[str]:
+    """Ouvre un fichier contenant une liste de mots et retourne cette liste.
+
+    Args:
+        path (str): Chemin vers le fichier à ouvrir.
+
+    Returns:
+        list[str]: Liste des mots extraits du fichier.
+
+    Raises:
+        FileNotFoundError: Si le fichier n'est pas trouvé.
+    """
     try:
         with open(path, 'r') as file:
             lines = file.readlines()
             words_list = [line.rstrip('\n') for line in lines]
         return words_list
     except FileNotFoundError:
-        print('language file not found')
+        print('Language file not found')
 
-def check_french_message(message:str) -> bool:
+def check_french_message(message: str) -> bool:
+    """Vérifie si un message est potentiellement en français
+    en comparant les mots à un dictionnaire de référence.
+
+    Args:
+        message (str): Message à vérifier.
+
+    Returns:
+        bool: True si le message semble être en français, sinon False.
+    """
     mess = message.split(' ')
-    for word in range(0,4):
+    for word in range(0, 4):
         if ("'" or "-" or "\n") not in mess[word]:
             if mess[word] not in constantes.DICO:
                 return False
     return True
-                
-#phase de test ==> appuyez sur la petite fleche en haut a droite
+
+# Phase de test
 if __name__ == '__main__':
-    #Dechiffrement message1
+    # Déchiffrement message1
     print('{0:=^120}'.format('MESSAGE-1'))
-    mot = crypto.Substition("message1_chiffre.txt")
+    mot = crypto.Substitution("message1_chiffre.txt")
     print(mot.decryptage_naif())
 
-    #Dechiffrement message2
+    # Déchiffrement message2
     print('{0:=^120}'.format('MESSAGE-2'))
-    mot = crypto.Substition("message2_chiffre.txt")
+    mot = crypto.Substitution("message2_chiffre.txt")
     print(mot.dechiffrement_vernam('CINQ'))
 
-    #Dechifrement message3
+    # Déchiffrement message3
     print('{0:=^120}'.format('MESSAGE-3'))
     chars_grid = "AJFB82YN9UX1GS0KPI3QOE74CZVHRLT5WD6M"
     public_key = 'CRYPTO'
-    test = crypto.Substition('message3_chiffre.txt')
-    decrypt_msg = test.dechiffrement_message_ADFGVX(chars_grid,public_key)
+    test = crypto.Substitution('message3_chiffre.txt')
+    decrypt_msg = test.dechiffrement_message_ADFGVX(chars_grid, public_key)
     print(decrypt_msg)
-
