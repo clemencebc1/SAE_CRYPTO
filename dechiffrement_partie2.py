1ed724ded2c2ad0c7e7db4426c03c039c5c7cfc2
clemence <clemence.bcq@gmail.com>
2024-12-01 20:21:59 +0100

    ajout de la fonction get_tcp

--- dechiffrement_partie2.py
+++ dechiffrement_partie2.py
@@ -12,13 +12,39 @@ def open_trace(fichier):
         print("Erreur ouverture fichier")
     
 def get_udp(packets):
-    for packet in packets:
-        if (UDP or TCP) in packet:
-            print(packet)
-            
+    """Recupere l'ensemble donnees des packets UDP
+
+    Args:
+        packets (PacketList): liste de packets
+
+    Returns:
+        dict: dictionnaire contenant les donnees udp
+    """
+    dico_packets_data = dict()
+    for i in range(len(packets)):
+        if UDP in packets[i]:
+            dico_packets_data[i] = bytes(packets[i][UDP].payload)
+    return dico_packets_data
+
+def get_tcp(packets):
+    """Recupere l'ensemble donnees des packets TCP
+
+    Args:
+        packets (PacketList): liste de packets
+
+    Returns:
+        dict: dictionnaire contenant les donnees TCP
+    """
+    dico_packets_data = dict()
+    for i in range(len(packets)):
+        if TCP in packets[i]:
+            dico_packets_data[i] = bytes(packets[i][TCP].payload)
+    return dico_packets_data
+    
+
 
     
     
-    
 packets = open_trace("gr_17/trace_1.pcap")
-get_udp(packets)
+print(get_udp(packets))
+
