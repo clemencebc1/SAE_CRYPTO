7325dcf75ff9f2a52af0ed9be6493c63fbddd89c
clemence <clemence.bcq@gmail.com>
2024-12-08 14:25:09 +0100

    ajout fonction chiffrement cbc

--- dechiffrement_partie2.py
+++ dechiffrement_partie2.py
@@ -49,42 +49,69 @@ def get_tcp(packets):
 def dechiffrement_cbc():
     pass
 
+def chiffrement_cbc(mot_a_chiffrer, cle=None):
+    """chiffrement d'un message en CBC
+
+    Args:
+        mot_a_chiffrer (str): image à chiffrer
+        cle (str, optional): cle (img precedente). Defaults to None.
+    """
+    cpt = 0 # compteur de parcours du vecteur initialisation
+    mot_a_chiffrer = Image.open(mot_a_chiffrer)
+    sortie = mot_a_chiffrer.copy()
+    for i in range(mot_a_chiffrer.size[1]):
+        for j in range(mot_a_chiffrer.size[0]):
+            if cpt >= len(cle):
+                cpt = 0
+            pixel = mot_a_chiffrer.getpixel((j,i))
+            if pixel%2 != 0 and cle[cpt] == 1:
+                sortie.putpixel((j,i), pixel+1)
+            elif pixel%2 == 0 and cle[cpt] != 1:
+                sortie.putpixel((j,i), pixel+1)
+            else: 
+                sortie.putpixel((j,i), pixel)
+        cpt += 1
+    sortie.save("./img/cle.png")
+
 def premiere_cle():
     binary_data = FIRST_PAQUET_DATA
     pixels = [int(bit) for bit in binary_data]
     pixel_matrix = np.array(pixels).reshape(32, 32)
     image = Image.fromarray((pixel_matrix * 255).astype('uint8'))
-    image.save("output_image.png")
+    image.save("vecteur_initialisation.png")
+
+def img_to_list(img):
+    """converti une image en une liste de valuers
+
+    Args:
+        img (str): lien img
+    """
+    image = Image.open(img)
+    liste = []
+    for i in range(image.size[1]):
+        for j in range(image.size[0]):
+            liste.append(image.getpixel((i,j)))
+    return liste
+
 
 def extract_key(image1_path, image2_path, output_path):
-    # Charger les images
     image1 = Image.open(image1_path)
     image2 = Image.open(image2_path)
 
-    # Convertir les images en tableaux numpy
     image1_array = np.array(image1)
     image2_array = np.array(image2)
 
-    # Calculer les différences pixel par pixel
-    pixel_differences = np.abs(image1_array - image2_array)
+    pixel_differences = np.abs(image1_array - image2_array) # différence entre pixels
 
     # Isoler les pixels non nuls comme la clé (seuil de 1)
     key_pixels = (pixel_differences > 0).astype(np.uint8) * 255
 
-    # Sauvegarder l'image résultante
     key_image = Image.fromarray(key_pixels)
     key_image.save(output_path)
     
 packets = open_trace("gr_17/trace_1.pcap")
 
-"1147f288500b1dd47605cd84c1af4a276352ab9a3e27de5868b9dfc1d5a044d928083a2fafc04f3c27be51fd6436aac6"
+"11 47 f2 88 50 0b 1d d4 76 05 cd 84 c1 af 4a 27 63 52 ab 9a 3e 27 de 58 68 b9 df c1 d5 a0 44 d9 28 08 3a 2f af c0 4f 3c 27 be 51 fd 64 36 aa c6"
 "43 11 79 9b 24 e1 09 6f bf b6 68 7a f8 76 49 f5 e0 10 84 7a 10 4c ac 93 dd e2 c7 99 5c 52 43 ff 28 08 3a 2f af c0 4f 3c 27 be 51 fd 64 36 aa c6"
-
-
-# Chemins des images et de la sortie
-image1_path = "rossignol.bmp"
-image2_path = "stegano_17.bmp"
-output_path = "key_difference.bmp"
-
-# Appeler la fonction pour extraire la clé
-extract_key(image1_path, image2_path, output_path)
+vecteur_initialisation = img_to_list("./img/vecteur_initialisation.png")
+print(chiffrement_cbc("./img/rossignol.bmp", vecteur_initialisation))
